957fbb9f35adffd6b3b0c1dadb5fd77b9c7379bd
inet.ke <ineta.keryte@gmail.com>
2026-03-18 18:31:32 +0000

    changed the price

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ MAPS_URL = "https://maps.app.goo.gl/GFaQENB6pXwxRyUL7?g_st=ic"
 BIZUM_PHONE = "+34 600 123 456"
 BANK_IBAN = "ES12 1234 0000 0000 0000 0000"
 PRICE_INDIVIDUAL = "20€"
-PRICE_PAIR = "40€ por pareja"
+PRICE_PAIR = "35€ por pareja"
 
 
 # ---------------- Secrets / Clients ----------------
