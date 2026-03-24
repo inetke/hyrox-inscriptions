fdff4c9412de4d95a517ced6e9d6f6c905c4eff0
inet.ke <ineta.keryte@gmail.com>
2026-03-24 12:03:45 +0000

    added info sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+st.info("Abre el menú lateral (arriba a la izquierda >>) para ver precios, ubicación y contacto.")
+
 # CSS (aplica a toda la app)
 st.markdown(
     """
