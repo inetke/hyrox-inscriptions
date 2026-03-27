e11c25db9150528300a862d04c8e6e17db274b60
inet.ke <ineta.keryte@gmail.com>
2026-03-27 22:26:08 +0000

    price

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ MAPS_URL = "https://maps.app.goo.gl/GFaQENB6pXwxRyUL7?g_st=ic"
 PAGO_EFECTIVO = "https://maps.app.goo.gl/qHpFpn4dkEvpHkt69"
 BANK_IBAN = "ES12 1234 0000 0000 0000 0000"
 PRICE_INDIVIDUAL = "20€"
-PRICE_PAIR = "35€ por pareja"
+PRICE_PAIR = "40€ por pareja"
 
 
 # ---------------- Secrets / Clients ----------------
