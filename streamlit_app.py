3b770e3efcfbfdbc272e75c433d11d2570c0bbe6
inet.ke <ineta.keryte@gmail.com>
2026-04-08 10:33:15 +0100

    from 100 to 75 places

--- streamlit_app.py
+++ streamlit_app.py
@@ -239,7 +239,7 @@ def fetch_total_remaining():
         else:
             occupied += 1
 
-    return 100 - occupied
+    return 75 - occupied
 
 
 # ---------------- Sidebar ----------------
@@ -427,7 +427,7 @@ with st.expander("Panel admin"):
         st.markdown("### Aforo del evento")
 
         remaining = fetch_total_remaining()
-        occupied = 100 - remaining
+        occupied = 75 - remaining
 
         #st.info(f"🎟️ Ocupadas: {occupied} | Disponibles: {remaining}/100")
         
