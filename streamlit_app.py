0c383f5ad947cdf189f65a66e25b60560e8ce329
inet.ke <ineta.keryte@gmail.com>
2026-04-20 22:45:47 +0100

    hided one warning

--- streamlit_app.py
+++ streamlit_app.py
@@ -354,7 +354,7 @@ with left:
         st.error("❌ Evento completo")
         st.stop()
 
-    st.info(f"🎟️ Plazas disponibles: {remaining}/100")
+    #st.info(f"🎟️ Plazas disponibles: {remaining}/100")
     st.info("📢 Una semana antes se les comunicará a qué tanda van a pertenecer.")
 
 
