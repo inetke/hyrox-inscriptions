612bba82c6a88ee3c8e0881f4d48cb0e01beef46
inet.ke <ineta.keryte@gmail.com>
2026-06-11 14:18:05 +0100

    delete bizum number

--- streamlit_app.py
+++ streamlit_app.py
@@ -200,7 +200,6 @@ MAPS_URL = "https://maps.app.goo.gl/GFaQENB6pXwxRyUL7?g_st=ic"
 PAGO_EFECTIVO = "https://maps.app.goo.gl/qHpFpn4dkEvpHkt69"
 PAGO_EFECTIVO_H = "https://maps.app.goo.gl/GFaQENB6pXwxRyUL7?g_st=ic"
 BANK_IBAN = "ES27 2100 6749 2702 0041 0384"
-#PAGO_BIZUM = "+34 659 09 22 27"
 ENTRADA_GENERAL = "25€ individual · 50€ dobles · 75€ tríos"
 ENTRADA_USUARIOS = "20€ individual · 40€ dobles · 60€ tríos"
 
